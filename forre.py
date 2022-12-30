70717e11b9d4cf86d5cd86d2e54a720813d3a8d6
Truongphi20 <96680644+Truongphi20@users.noreply.github.com>
2022-12-30 17:29:05 +0700

    Sai ham dau si

--- forre.py
+++ forre.py
@@ -61,7 +61,75 @@ def dausis(l1,l2): # Tim list phan tich cua 2 list
 		#print(dau1_index,dau2_index)
 	return rs.tolist()[1:] 
 
+#print(dausis([1],[1,1,1]))
 
+def leng(lista): # khoang cach cac diem
+	lengths =[0]*(len(lista)-1)
+	for i in range(len(lista)-1):
+		lengths[i] = lista[i+1][0]-lista[i][0]
+	return lengths
+
+def Reparelib(lista): # Chuan bi thu vien
+
+	lib_vals = {ele[0]:ele[1] for ele in lista}
+	#print(lib_vals)
+
+	keys = [key for key in lib_vals]
+
+	for k in range(len(keys)-1):
+		lib_vals[keys[len(keys)-1-k]] =  lib_vals[keys[len(keys)-1-k]] - lib_vals[keys[len(keys)-2-k]]
+	lib_vals.pop(keys[0])
+	#print(lib_vals)
+
+	lengths = leng(lista)	# Do dai
+	#print(lengths)
+
+	new_keys = keys[1:]
+	#print(new_keys)
+
+	for length, key in zip(lengths,new_keys):
+		lib_vals[key] = (lib_vals[key]/length,[1]*length)
+	#print(lib_vals)
+
+	return lib_vals
+
+def Caculate(lib_vals): # Giai hist thuc 1 bac
+	keys = [key for key in lib_vals]
+	lib_tem = {}
+	for k in range(len(keys)-1):
+		a_val = lib_vals[keys[k]][0]
+		a_lis = lib_vals[keys[k]][1]
+		a_length =  sum(a_lis)
+
+		b_val = lib_vals[keys[k+1]][0]
+		b_lis = lib_vals[keys[k+1]][1]
+		b_length =  sum(b_lis)
+		
+		new_lis = dausis(a_lis,b_lis)
+		#print(new_lis)
+		new_val = (b_val - a_val)*b_length*a_length/sum(new_lis)
+		
+		lib_tem[keys[k+1]] = (new_val,new_lis)
+
+	lib_vals = lib_tem
+	#print(lib_vals)
+	return lib_vals
+
+def HitThuc(lista): # Tinh hist thuc
+	lib_vals = Reparelib(lista)
+	print(lib_vals)
+
+	lib_vals = Caculate(lib_vals)
+	print(lib_vals)
+
+	lib_vals = Caculate(lib_vals)
+	print(lib_vals)
+
+	lib_vals = Caculate(lib_vals)
+	print(lib_vals)
+
+vals = [(1,0),(3,24),(4,60),(7,336),(8,504),(10,990)]
+HitThuc(vals)
 
 
 
--- test_forre.py
+++ test_forre.py
@@ -8,6 +8,7 @@ class TestForre(unittest.TestCase):
 		self.hs2 = [1,3,1]	#4,5,6
 		self.val1 = 26
 		self.val2 = 16
+		self.l_vals = [(0,0),(2,4),(3,18),(6,180),(8,448)] # list toa do cua cac diem (x,y)
 	
 	def tearDown(self):
 		print("Tear Down")
@@ -19,17 +20,22 @@ class TestForre(unittest.TestCase):
 
 	def test_dienso(self):
 		print("dienso")
-		print("#1")
 		self.assertEqual(fo.dienso(1,5,3,[0,0,0,0,0,0,0,0]),[0,0,3,3,3,3,0,0])
-		print("#2")
 		self.assertEqual(fo.dienso(2,4,3,[0,0,0,0,0]),[0,0,0,3,3])
 
 	def test_dausi(self):
 		print("dausi")
-		print('#1')
 		self.assertEqual(fo.dausis(self.hs2,self.hs1),[3,12,15,5])
-		print('#2')
 		self.assertEqual(fo.dausis(self.hs1,self.hs2),[10,15,12,3])
+		self.assertEqual(fo.dausis([1],[1]),[1])
+
+	def test_leng(self):
+		print("leng")
+		self.assertEqual(fo.leng(self.l_vals),[2,1,3,2])
+
+	def test_Reparelib(self):
+		print("Reparelib")
+		self.assertEqual(fo.Reparelib(self.l_vals),{2:(2,[1,1]),3:(14,[1]),6:(54,[1,1,1]),8:(134,[1,1])})
 
 if __name__ == '__main__':
 	unittest.main()
